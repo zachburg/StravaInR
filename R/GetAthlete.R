87c05aefd07940ac04613af7d1de697653d63907
Zach Burgess <zachburg@uw.edu>
2016-07-21 22:15:11 -0700

    Updated GetAthlete

--- R/GetAthlete.R
+++ R/GetAthlete.R
@@ -1,12 +1,13 @@
-GetAthlete <- function(athleteId) {
+GetAthlete <- function(id) {
   reqUri <- "https://www.strava.com/api/v3/athlete";
-  if (!missing(athleteId)) {
-    reqUri <- paste(reqUri, "s/", athleteId, sep="")
+  if (!missing(id)) {
+    reqUri <- paste(reqUri, "s/", id, sep="")
   }
+  reqUri <- paste(reqUri, "/?resource_state=", resource.state, sep="")
   request <- GET(reqUri, config=config(token=token))
   if (request$status_code != 200) {
     print("Something wrong")
-    return;
+    return();
   }
   return(content(request))
 }
