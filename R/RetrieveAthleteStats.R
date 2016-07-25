e5ab600bb6e8f02d1753139c4c5b455093fd438f
Zach Burgess <zachburg@uw.edu>
2016-07-24 22:13:58 -0700

    Moved files from R/base to R directory

--- R/RetrieveAthleteStats.R
+++ R/RetrieveAthleteStats.R
@@ -1,6 +1,6 @@
 RetrieveAthleteStats <- function(id) {
   reqUri <- paste("https://www.strava.com/api/v3/athletes/", id, "/stats",
                   sep = "")
-  request <- GET(reqUri, config=config)
+  request <- GET(reqUri, config=config(token=token))
   return(content(request))
 }
